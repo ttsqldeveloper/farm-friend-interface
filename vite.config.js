6794c36b771210ae2e34adb894f3ca48448c41ed
ttsqldeveloper <ttsqldeveloper@gmail.com>
2026-04-29 00:18:58 +0200

    fix: use correct react-swc plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
